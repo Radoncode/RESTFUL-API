ebdaa914e2bfc502152c0b2226dc7380c16f14b9
Radoncode <relbouni69@gmail.com>
2022-07-04 21:45:26 +0100

    learning how to use CURLOPT_HEADERFUNCTION

--- index.php
+++ index.php
@@ -4,17 +4,36 @@ require '.env';
 
 $ch = curl_init();
 
-
 $headers = [
     "Authorization: Client-ID ".API_KEY_HERE
 ];
+
+$response_headers = [];
+
+$header_callback = function($ch, $header) use (&$response_headers) {
+
+    $len = strlen($header);
+
+    $parts = explode(":", $header, 2);
+
+    if (count($parts) < 2) {
+        
+        return $len;
+
+    }
+
+    $response_headers[$parts[0]] = trim($parts[1]);
+
+    return $len;
+
+};
 
 curl_setopt_array($ch, [
 
     CURLOPT_URL => "https://api.unsplash.com/photos/random",
     CURLOPT_RETURNTRANSFER => true,
     CURLOPT_HTTPHEADER => $headers,
-    CURLOPT_HEADER => true
+    CURLOPT_HEADERFUNCTION => $header_callback
 
 ]);
 
@@ -22,14 +41,10 @@ $response = curl_exec($ch);
 
 $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 
-$content_type = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
-
-$content_length = curl_getinfo($ch, CURLINFO_CONTENT_LENGTH_DOWNLOAD);
-
 curl_close($ch);
 
 echo $status_code, "\n";
-echo $content_type, "\n";
-echo $content_length, "\n";
+
+print_r($response_headers);
 
 echo $response, "\n";
