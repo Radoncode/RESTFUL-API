3d73f08de9db387ed5bc1b9ba566f7de37aebf80
Radoncode <relbouni69@gmail.com>
2022-07-04 22:40:52 +0100

    How to use CURLOPT_CUSTOMREQUEST

--- index.php
+++ index.php
@@ -5,15 +5,16 @@ require '.env';
 $ch = curl_init();
 
 $headers = [
-    "Authorization: token ".YOUR_ACCESS_KEY,
-    //"User-Agent: Radoncode"
+    "Authorization: token ".YOUR_ACCESS_KEY
 ];
 
 curl_setopt_array($ch, [
 
     CURLOPT_URL => "https://api.github.com/user/starred/Radoncode/API-PHP",
     CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_HTTPHEADER => $headers
+    CURLOPT_HTTPHEADER => $headers,
+    CURLOPT_USERAGENT => "Radoncode",
+    CURLOPT_CUSTOMREQUEST => "PUT"
 
 ]);
 
