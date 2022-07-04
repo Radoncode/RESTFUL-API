8f258a6c4c1ac173ff986ca0d7a93ecfff7b066c
Radoncode <relbouni69@gmail.com>
2022-07-04 20:07:39 +0100

    Learn status code and retrieving datas of weather

--- index.php
+++ index.php
@@ -6,12 +6,16 @@ $ch = curl_init();
 //curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
 curl_setopt_array($ch, [
-    CURLOPT_URL => "https://randomuser.me/api",
+    CURLOPT_URL => "https://api.openweathermap.org/data/2.5/weather?q=London&appid=31d1f2a16aa92ea8f0603d5aa61158ac",
     CURLOPT_RETURNTRANSFER => true
 ]);
 
 $response = curl_exec($ch);
 
+$status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
 curl_close($ch);
+
+echo $status_code, "\n";
 
 echo $response, "\n";
