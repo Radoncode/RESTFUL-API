4463a7907eceb53823f2996699622556c4b2d7d9
Radoncode <relbouni69@gmail.com>
2022-07-05 18:35:40 +0100

    using front controller to create api

---
+++ api/index.php
@@ -0,0 +1,33 @@
+<?php
+
+require '../.env';
+
+
+$path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+
+$parts = explode("/", $path);
+
+$resource = $parts[2];
+
+$id = $parts[3] ?? null;
+
+if ($resource != "tasks") {
+
+    //header("{$_SERVER['SERVER_PROTOCOL']} 404 Not Found");
+    http_response_code(404);
+    exit;
+}
+
+require dirname(__DIR__). "/src/TaskController.php";
+
+$controller = new TaskController();
+
+$controller->processRequest($_SERVER['REQUEST_METHOD'], $id);
+
+
+
+
+
+
+
+
